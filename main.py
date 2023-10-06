ce9a91fbab06d20b999b2a572df0a5903c6fd087
Grzesiek <gromaniuk0@gmail.com>
2023-10-06 16:34:44 +0200

    More unit tests added

--- main.py
+++ main.py
@@ -20,22 +20,29 @@ class Expense:
     id: int
     amount: float
     description: str
-    
+
     def __post_init__(self):
         if float(self.amount) < 0:
             raise ValueError("Koszt nie może być ujemny")
         if not self.description:
             raise ValueError("Opis nie może być pusty")
+
     def __eq__(self, other):
-        return self.id == other.id and self.amount == other.amount and self.description == other.description
-        
+        return (
+            self.id == other.id
+            and self.amount == other.amount
+            and self.description == other.description
+        )
+
     def __post_repr__(self):
         return f"Expense(id={self.id!r}, description={self.description!r}, amount={self.amount!r})"
+
 
 @dataclass
 class CSV_import:
     amount: float
     description: str
+
     def __post_init__(self):
         if self.amount[0] == "-":
             raise ValueError("Koszt nie może być ujemny")
@@ -50,7 +57,7 @@ class CSV_import:
                     if counter > 1:
                         raise ValueError("Wszystkie koszty muszą być liczbami")
                 elif not each.isnumeric():
-                        raise ValueError("Wszystkie koszty muszą być liczbami")
+                    raise ValueError("Wszystkie koszty muszą być liczbami")
 
         if not self.description:
             raise ValueError("Opis nie może być pusty")
@@ -64,15 +71,13 @@ def find_next_id(expense_list: list[Expense]):
     return counter
 
 
-
 def read_db_or_init() -> list[Expense]:
     try:
         with open(DB_FILENAME, "rb") as stream:
             expense_list = pickle.load(stream)
     except FileNotFoundError:
-            expense_list = []
+        expense_list = []
     return expense_list
-
 
 
 def save_db(expense_list: list[Expense], overwrite: bool = True) -> None:
@@ -80,27 +85,35 @@ def save_db(expense_list: list[Expense], overwrite: bool = True) -> None:
     with open(DB_FILENAME, mode) as stream:
         pickle.dump(expense_list, stream)
 
+
 def add_expense(expense_list: list[Expense], amount: float, description: str) -> None:
     expense_item = Expense(
-        id=find_next_id(expense_list),
-        amount=amount,
-        description=description
+        id=find_next_id(expense_list), amount=amount, description=description
     )
     expense_list.append(expense_item)
 
+
 def create_Expense_item_from_dict(row: dict[str, str]) -> [CSV_import]:
     try:
-        return CSV_import(description=row['description'], amount=(row['amount']))
+        return CSV_import(description=row["description"], amount=(row["amount"]))
     except KeyError:
         print("Błąd pliku - akceptowalne pliki z kluczami 'amount' oraz 'description'")
         sys.exit(1)
+
 
 def read_expenses(filename: str, expense_list) -> list[Expense]:
     with open(filename, encoding="utf-8") as stream:
         reader = csv.DictReader(stream)
         try:
             expenses_no_id = [create_Expense_item_from_dict(row) for row in reader]
-            expenses_csv = [Expense(find_next_id(expense_list), float(expense.amount), expense.description) for expense in expenses_no_id]
+            expenses_csv = [
+                Expense(
+                    find_next_id(expense_list),
+                    float(expense.amount),
+                    expense.description,
+                )
+                for expense in expenses_no_id
+            ]
             return expenses_csv
         except ValueError as f:
             print(f"Błąd - {f.args[0]}")
@@ -108,39 +121,46 @@ def read_expenses(filename: str, expense_list) -> list[Expense]:
 
 
 def add_csv_to_db(csv_file):
+    # Tested manually, csv content successfully added to db.
+    # Exceptions correctly raised for incorrect data format or missing columns with appropriate messages.
     expense_list = read_db_or_init()
     expenses_csv = read_expenses(csv_file, expense_list)
     [expense_list.append(each) for each in expenses_csv]
     return expense_list
 
+
 def strip_zeros(number: float) -> str:
-    return str(number).rstrip('0').rstrip('.') if '.' in str(number) else number
-    
+    return str(number).rstrip("0").rstrip(".") if "." in str(number) else str(number)
+
 
 def print_expenses(expense_list: list[Expense]) -> None:
-    print(expense_list)
     print(f"==ID==  ==Amount==  =BIG?=  =DESCRIPTION=")
     total = 0
     for item in expense_list:
         total += float(item.amount)
-        if item.amount >= 1000:  #float?
+        if item.amount >= 1000:  # float?
             big = "(!)"
         else:
             big = " "
-        print(f"{item.id:^6}    {strip_zeros(item.amount):<8}   {big:^6}  {item.description:<20}")
-    print("TOTAL:   ",f"{strip_zeros(total)}")
+        print(
+            f"{item.id:^6}    {strip_zeros(item.amount):<8}   {big:^6}  {item.description:<20}"
+        )
+    print("TOTAL:   ", f"{strip_zeros(total)}")
+
 
 @click.group()
 def clack():
     pass
+
 
 @clack.command()
 def export_python():
     expense_list = read_db_or_init()
     print(repr(expense_list))
 
+
 @clack.command
-@click.argument('csv_file')
+@click.argument("csv_file")
 def import_csv(csv_file):
     expense_list = add_csv_to_db(csv_file)
     save_db(expense_list)
@@ -152,12 +172,13 @@ def report() -> None:
     expenses = read_db_or_init()
     print_expenses(expenses)
 
+
 @clack.command()
 @click.argument("amount")
 @click.argument("description")
 def add(amount: float, description: str) -> None:
     try:
-        amount = float(amount.replace(",","."))
+        amount = float(amount.replace(",", "."))
     except ValueError:
         print("Błąd - Koszt musi być liczbą")
         sys.exit(1)
@@ -170,18 +191,18 @@ def add(amount: float, description: str) -> None:
     save_db(expense_list)
     print("Dodano")
 
+
 if __name__ == "__main__":
-    clack()
-
-
+    expense_list = [Expense(1, 123.00, "Cherry"), Expense(2, 1000, "Garden swing")]
+    print_expenses(expense_list)
 
 
 # Napisz program, który ułatwi milionom Polaków śledzenie własnych wydatków oraz ich analizę. Program pozwala na łatwe dodawanie nowych wydatków i generowanie raportów. Aplikacja działa także pomiędzy uruchomieniami, przechowując wszystkie dane w pliku. Każdy wydatek ma id, opis oraz wielkość kwoty.
 
-# 1. Program posiada podkomendy add, report, export-python oraz import-csv. 
+# 1. Program posiada podkomendy add, report, export-python oraz import-csv.
 
 # 2. Podkomenda add pozwala na dodanie nowego wydatku. Należy wówczas podać jako kolejne argumenty wiersza poleceń wielkość wydatku (jako int) oraz jego opis (w cudzysłowach). Na przykład:
-# $ python budget.py add 100 "stówa na zakupy". 
+# $ python budget.py add 100 "stówa na zakupy".
 # Jako id wybierz pierwszy wolny id - np. jeśli masz już wydatki z id = 1, 2, 4, 5, wówczas wybierz id = 3.
 
 # 3. Podkomenda report wyświetla wszystkie wydatki w tabeli. W tabeli znajduje się także kolumna "big?", w której znajduje się ptaszek, gdy wydatek jest duży, tzn. co najmniej 1000. Dodatkowo, na samym końcu wyświetlona jest suma wszystkich wydatów.
--- test_Budget.py
+++ test_Budget.py
@@ -5,6 +5,8 @@ from main import (
     add_expense,
     create_Expense_item_from_dict,
     read_expenses,
+    print_expenses,
+    add_csv_to_db,
     strip_zeros,
     import_csv,
     Expense,
@@ -26,14 +28,12 @@ class TestExpenseClass(unittest.TestCase):
     def tearDown(self) -> None:
         return super().tearDown()
 
-
     def test_negative_amount(self):
         with self.assertRaises(ValueError) as message:
             expense = Expense(id=1, amount=-1, description="Negative Expense")
             expense.__post_init__()
-        
-        self.assertEqual(str(message.exception), "Koszt nie może być ujemny")
 
+        self.assertEqual(str(message.exception), "Koszt nie może być ujemny")
 
     def test_empty_string(self):
         with self.assertRaises(ValueError) as message:
@@ -48,24 +48,23 @@ class TestCsvImportClass(unittest.TestCase):
 
     def tearDown(self) -> None:
         return super().tearDown()
-    
+
     def test_correct_input(self):
         instance = CSV_import(amount="100.50", description="Valid Expense")
         self.assertIsInstance(instance, CSV_import)
-
 
     def test_negative_amount(self):
         with self.assertRaises(ValueError) as message:
             expense = CSV_import(amount="-1", description="Negative Expense")
             expense.__post_init__()
-        
+
         self.assertEqual(str(message.exception), "Koszt nie może być ujemny")
 
     def test_wrong_format_value(self):
         with self.assertRaises(ValueError) as message:
             expense = CSV_import(amount="Pax", description="Amount NaN")
             expense.__post_init__()
-        
+
         self.assertEqual(str(message.exception), "Wszystkie koszty muszą być liczbami")
 
     def test_empty_string(self):
@@ -107,58 +106,21 @@ class TestReadDB(unittest.TestCase):
 
     def tearDown(self) -> None:
         return super().tearDown()
-    
+
     @patch("pickle.load")
     @patch("builtins.open", new_callable=mock_open)
     def test_read_db_or_init_with_db(self, mock_file, mock_load):
-        # The mock data that we expect from the database
-        mock_expenses = [
-            Expense(id=1, amount=234.0, description="Zabawki")
-        ]
-
-        # Set the return value for the mock pickle.load function
+        mock_expenses = [Expense(id=1, amount=234.0, description="Zabawki")]
         mock_load.return_value = mock_expenses
-
-        # Call the actual function we want to test
         got = read_db_or_init()
-
-        # Assert that our function returns the mocked data
         self.assertEqual(got, mock_expenses)
-
 
     @patch("pickle.load")
     def test_read_db_or_init_no_db(self, mock_file):
         mock_file.side_effect = FileNotFoundError
         got = read_db_or_init()
         self.assertEqual(got, [])
-        
 
-class TestReadDataBase(unittest.TestCase):
-
-    def test_read_db_or_init_with_db(self):
-        # The mock data that we expect from the database
-        mock_expenses = [
-            Expense(id=1, amount=234.0, description="Zabawki"),
-            Expense(id=2, amount=334.0, description="Fish"),
-            Expense(id=3, amount=76.23, description="Yt premium")
-        ]
-
-        # Serialize mock_expenses to simulate reading from a file.
-        mock_data = pickle.dumps(mock_expenses)
-
-        # Mocking open
-        m = mock_open(read_data=mock_data)
-
-        with patch('builtins.open', m):
-            # Mocking pickle.load
-            with patch('pickle.load') as mock_load:
-                mock_load.return_value = mock_expenses
-                
-                # Call the actual function we want to test
-                got = read_db_or_init()
-
-                # Assert that our function returns the mocked data
-                self.assertEqual(got, mock_expenses)
 
 class TestAddExpense(unittest.TestCase):
     def setUp(self) -> None:
@@ -166,7 +128,7 @@ class TestAddExpense(unittest.TestCase):
 
     def tearDown(self) -> None:
         return super().tearDown()
-    
+
     def test_add_expense_single(self):
         expense_list = []
         add_expense(expense_list, amount=60, description="Pendrive")
@@ -175,7 +137,7 @@ class TestAddExpense(unittest.TestCase):
         self.assertEqual(added_expense.amount, 60)
         self.assertEqual(added_expense.description, "Pendrive")
         self.assertEqual(added_expense.id, 1)
-    
+
     def test_add_expense_multiple(self):
         expense_list = [(Expense(2, 22, "Dwójka"))]
         add_expense(expense_list, amount=60, description="Pendrive")
@@ -197,48 +159,52 @@ class TestCreateExpenseItemFromDict(unittest.TestCase):
     def test_valid_input(self):
         row = {"amount": "60", "description": "Pendrive"}
         expense = create_Expense_item_from_dict(row)
-        
+
         self.assertIsInstance(expense, CSV_import)
         self.assertEqual(expense.amount, "60")
         self.assertEqual(expense.description, "Pendrive")
 
     def test_missing_amount_key(self):
         row = {"description": "Pendrive"}
-        
+
         with self.assertRaises(SystemExit) as message:
             create_Expense_item_from_dict(row)
-        
+
         self.assertEqual(message.exception.code, 1)
 
     def test_missing_description_key(self):
         row = {"amount": "60"}
-        
+
         with self.assertRaises(SystemExit) as message:
             create_Expense_item_from_dict(row)
-        
+
         self.assertEqual(message.exception.code, 1)
 
     def test_invalid_amount(self):
         row = {"amount": "abc", "description": "Invalid Amount"}
-        
+
         with self.assertRaises(ValueError) as message:
             create_Expense_item_from_dict(row)
-        
+
         self.assertEqual(str(message.exception), "Wszystkie koszty muszą być liczbami")
 
-class TestReadExpenses(unittest.TestCase):
 
+class TestReadExpenses(unittest.TestCase):
     def setUp(self) -> None:
         return super().setUp()
 
     def tearDown(self) -> None:
         return super().tearDown()
 
-    @patch('M07L12_projekt.create_Expense_item_from_dict', side_effect=create_Expense_item_from_dict)
-    @patch('M07L12_projekt.find_next_id', side_effect=find_next_id)
-    @patch('builtins.open', create=True)
+    @patch(
+        "main.create_Expense_item_from_dict", side_effect=create_Expense_item_from_dict
+    )
+    @patch("main.find_next_id", side_effect=find_next_id)
+    @patch("builtins.open", create=True)
     def test_valid_input(self, mock_open, mock_create_expense, mock_find_id):
-        mock_open.return_value.__enter__.return_value = StringIO("amount,description\n60,Pendrive\n40,USB Cable")
+        mock_open.return_value.__enter__.return_value = StringIO(
+            "amount,description\n60,Pendrive\n40,USB Cable"
+        )
 
         expense_list = []
 
@@ -250,12 +216,15 @@ class TestReadExpenses(unittest.TestCase):
         self.assertEqual(expenses[0].amount, 60)
         self.assertEqual(expenses[0].description, "Pendrive")
 
-
-    @patch('M07L12_projekt.create_Expense_item_from_dict', side_effect=create_Expense_item_from_dict)
-    @patch('M07L12_projekt.find_next_id', side_effect=find_next_id)
-    @patch('builtins.open', create=True)
+    @patch(
+        "main.create_Expense_item_from_dict", side_effect=create_Expense_item_from_dict
+    )
+    @patch("main.find_next_id", side_effect=find_next_id)
+    @patch("builtins.open", create=True)
     def test_invalid_input(self, mock_open, mock_create_expense, mock_find_id):
-        mock_open.return_value.__enter__.return_value = StringIO("amount,description\nWrong data")
+        mock_open.return_value.__enter__.return_value = StringIO(
+            "amount,description\nWrong data"
+        )
 
         expense_list = []
 
@@ -266,5 +235,74 @@ class TestReadExpenses(unittest.TestCase):
             self.assertEqual(context.code, 1)
 
 
+class TestStripZeroes(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def test_number_following_zeroes(self):
+        got = strip_zeros(34.00)
+        expected = "34"
+        self.assertEqual(got, expected)
+
+    def test_number_no_following_zeroes(self):
+        got = strip_zeros(34)
+        expected = "34"
+        self.assertEqual(got, expected)
+
+
+class TestPrintExpenses(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def test_print_expenses(self):
+        expense_list = [Expense(1, 123.00, "Cherry"), Expense(2, 1000, "Garden swing")]
+
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        print_expenses(expense_list)
+        output = captured_output.getvalue()
+
+        sys.stdout = sys.__stdout__
+
+        self.assertIn("==ID==  ==Amount==  =BIG?=  =DESCRIPTION=", output)
+        self.assertIn("  2       1000        (!)    Garden swing", output)
+        self.assertIn("  1       123                Cherry", output)
+        self.assertIn("TOTAL:    1123", output)
+
+
 if __name__ == "__main__":
     unittest.main()
+
+
+# class TestAddCsvToDb(unittest.TestCase):
+
+#     def setUp(self) -> None:
+#         return super().setUp()
+
+#     def tearDown(self) -> None:
+#         return super().tearDown()
+
+#     @patch('main.read_expenses', side_effect = read_expenses)
+#     @patch('main.read_db_or_init', side_effect = read_db_or_init)
+#     def test_add_csv_to_db(self, mock_read_exp, mock_read_db):
+#         csv_content = "amount,description\n60,Pendrive\n40,USB Cable"
+
+#         # Mock the open function to return the CSV content
+#         with patch('builtins.open', create=True) as mock_open:
+#             mock_open.return_value.__enter__.return_value = StringIO(csv_content)
+
+#             # Call the function
+#             expenses = add_csv_to_db("temp_expenses.csv")
+
+#             # Assert that expenses contains the expected Expense objects
+#             self.assertEqual(expenses, [
+#                 Expense(id=1, amount=60.0, description='Pendrive'),
+#                 Expense(id=2, amount=40.0, description='USB Cable')
+#             ])
