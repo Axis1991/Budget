263c847ed045410f075d6d400c8ebf413127a682
Grzesiek <gromaniuk0@gmail.com>
2023-11-09 12:02:14 +0100

    Reduced CliRunner reduncancy with pytest.fixture

--- test_Budget_pytest.py
+++ test_Budget_pytest.py
@@ -23,14 +23,11 @@ MOCK_OUTPUT_2_ITEMS = [
 # read_db_or_init, find_next_id, add_csv_to_db, print_expenses, add_expense, save_db, - potentially to be tested
 
 @pytest.fixture
-def mock_expense_list():
-    """I will think what to do with this"""
-    return
+def runner():
+   return CliRunner() 
 
-
-def test_export_python():
+def test_export_python(runner):
     """ Test for returning correct repr string"""
-    runner = CliRunner()
     with patch("main.read_db_or_init", return_value=MOCK_OUTPUT_2_ITEMS):
         result = runner.invoke(export_python)
         printed_output = result.output.strip()
@@ -38,10 +35,9 @@ def test_export_python():
         assert result.exit_code == 0
 
 
-def test_import_csv():
+def test_import_csv(runner):
     """ Test to check if csv file is imported correctly and
      if there is appropriate interaction with database"""
-    runner = CliRunner()
     with tempfile.NamedTemporaryFile(delete=False, mode="w", suffix=".csv") as temp_csv:
         temp_csv.write("amount,description\n34,Hit Box\n453,Crazy curry\n")
 
@@ -55,11 +51,9 @@ def test_import_csv():
     os.remove(temp_csv.name)
 
 
-def test_report():
+def test_report(runner):
     """ Test to check if click module runs correctly
       correctly and function output is printed as expected"""
-    runner = CliRunner()
-
     with patch("main.read_db_or_init", return_value=MOCK_OUTPUT_2_ITEMS):
         result = runner.invoke(report)
         printed_output = result.output
@@ -70,13 +64,12 @@ def test_report():
         assert result.exit_code == 0
 
 
-def test_add():
+def test_add(runner):
     """ Test verifying that add function correctly adds element to a class
       and then to the list of items in the database"""
     test_amount = 123
     test_description = "Chicken"
     expense_list = []
-    runner = CliRunner()
     with patch("main.read_db_or_init", return_value=expense_list), patch(
         "main.save_db") as mock_save_db:
         result = runner.invoke(add, [str(test_amount), test_description])
